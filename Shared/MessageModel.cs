8e1945ea2add9fa944d0f24f32994adcdb44b7ff
Bananasplit393 <202305666@post.au.dk>
2025-04-07 11:24:21 +0200

    rm all files from git

--- Shared/MessageModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Shared; 
-
-class MessageModel
-{
-    public string? Message { get; set; }
-    public DateTime Time { get; set; }
-
-}
--- Shared/Models/Message.cs
+++
@@ -1,7 +0,0 @@
-namespace Shared.Models;
-
-public class Message
-{
-    public DateTime Timestamp { get; set; }
-    public int Counter { get; set; }
-} 
